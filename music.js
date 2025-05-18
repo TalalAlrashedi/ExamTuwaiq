a94b703e6532c0e3c3011645c6a97b177545aa3a
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-05-18 13:54:31 +0300

    ff

--- music.js
+++ music.js
@@ -1,24 +1,17 @@
 //api key f7398bf3-e79e-4817-a92c-f0072c868dec
 
 const getArtist = async () => {
-  try {
-    let res = await fetch(
-      "https://api.harvardartmuseums.org/object?apikey=f7398bf3-e79e-4817-a92c-f0072c868dec"
-    );
-    let data = await res.json();
-    // let names = data.people
+  let res = await fetch(
+    "https://api.harvardartmuseums.org/object?apikey=f7398bf3-e79e-4817-a92c-f0072c868dec"
+  );
+  let data = await res.json();
 
-    // names.forEach(name => {
-    //     console.log(name);
+//   data.records.forEach((element) => {
+//     // console.log(element.people);
+//     let names = element.people.forEach((e) => {
+//         console.log(e.name);
         
-    // });
-    console.log(data.people.name);
-    
-    // data.forEach(art => {
-    //     console.log(art.info);
-        
-    // });;
-    
-  } catch {}
+//);
+
 };
 getArtist();
